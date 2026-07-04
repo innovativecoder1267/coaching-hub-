4a35f3d5018000f86df198e2fdc808db8129b8d4
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-07-04 14:37:54 +0530

    Update route.ts

--- app/api/submitfee/route.ts
+++ app/api/submitfee/route.ts
@@ -1,7 +1,8 @@
-import supabase from "@/lib/supabase/supabase.client";
+import { createClient } from "@/lib/supabase/supabase.client";
 
 
 export async function POST(request:Request){
+    const supabase=createClient()
     const {student,amount,method}=await request.json();
     const {data,error}=await supabase.from("fees").insert({
         student,
