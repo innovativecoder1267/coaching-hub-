06801f1b3f578373d75d6e9a20a921f29d9c9eb1
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-07-13 16:45:06 +0530

    Add files via upload

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@/lib/supabase/supabase.client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+
+  const code = searchParams.get("code");
+
+  if (code) {
+    const supabase = await createClient();
+
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(
+    `${origin}/email-verified`
+  );
+}
